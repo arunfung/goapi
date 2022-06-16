c9b74c4bbe0172054b94b577c6c5d5ce9a42b86b
arun fung <arunfung@gmail.com>
2022-06-16 23:38:52 +0800

    验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	v1 "goapi/app/http/controllers/api/v1"
 	"goapi/app/models/user"
+	"goapi/app/requests"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,14 +17,22 @@ type SignupController struct {
 }
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
+
 	if err := c.ShouldBindJSON(&request); err != nil {
 		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		fmt.Println(err.Error())
+		return
+	}
+
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	// errs 返回长度等于零即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
 		return
 	}
 	//  检查数据库并返回响应
