1dc9824dcbb31621bdce137d8f6483995c0e6015
arun fung <arunfung@gmail.com>
2022-06-21 00:05:20 +0800

    图片验证码接口

--- config/app.go
+++ config/app.go
@@ -7,7 +7,7 @@ func init() {
 	config.Add("app", func() map[string]interface{} {
 		return map[string]interface{}{
 			// 应用名称
-			"name": config.Env("APP_NAME", "GoApi"),
+			"name": config.Env("APP_NAME", "go_api"),
 
 			// 当前环境，用以区分多环境，一般为 local, test, stage, production
 			"env": config.Env("APP_ENV", "production"),
