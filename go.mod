2e436a740fc0aeee16477e66781902fdfa274223
arun fung <arunfung@gmail.com>
2022-07-05 02:05:21 +0800

    限流中间件

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/cast v1.5.0
 	github.com/spf13/viper v1.12.0
 	github.com/thedevsaddam/govalidator v1.9.10
+	github.com/ulule/limiter/v3 v3.10.0
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
@@ -28,6 +29,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
@@ -46,6 +48,7 @@ require (
 	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
--- routes/api.go
+++ routes/api.go
@@ -13,6 +13,12 @@ import (
 func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
+
 	{
 		v1.GET("/test_auth", middlewares.AuthJWT(), func(c *gin.Context) {
 			userModel := pkgAuth.CurrentUser(c)
@@ -22,38 +28,44 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			c.String(http.StatusOK, "Hello guest")
 		})
 		authGroup := v1.Group("/auth")
+
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
+		authGroup.Use(middlewares.LimitIP("1000-H"))
+
 		{
+
 			suc := new(auth.SignupController)
 			// 判断手机是否已注册
-			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			authGroup.POST("/signup/phone/exist", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), suc.IsPhoneExist)
 			// 判断 Email 是否已注册
-			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			authGroup.POST("/signup/email/exist", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), suc.IsEmailExist)
 			// 手机号注册
-			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-phone", middlewares.GuestJWT(), suc.SignupUsingPhone)
 			// 邮箱注册
-			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
+			authGroup.POST("/signup/using-email", middlewares.GuestJWT(), suc.SignupUsingEmail)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
-			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("50-H"), vcc.ShowCaptcha)
 			// 发送短信验证码
-			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), vcc.SendUsingPhone)
 			// 发送邮箱验证码
-			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
+			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("20-H"), vcc.SendUsingEmail)
 
 			lgc := new(auth.LoginController)
 			// 使用手机号，短信验证码进行登录
-			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
+			authGroup.POST("/login/using-phone", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), lgc.LoginByPhone)
 			// 支持手机号，Email 和 用户名
-			authGroup.POST("/login/using-password", lgc.LoginByPassword)
+			authGroup.POST("/login/using-password", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), lgc.LoginByPassword)
 			// 刷新 Access Token
-			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+			authGroup.POST("/login/refresh-token", middlewares.AuthJWT(), lgc.RefreshToken)
 
 			// 重置密码
 			pwc := new(auth.PasswordController)
-			authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
-			authGroup.POST("/password-reset/using-email", pwc.ResetByEmail)
+			authGroup.POST("/password-reset/using-phone", middlewares.LimitPerRoute("60-H"), middlewares.GuestJWT(), pwc.ResetByPhone)
+			authGroup.POST("/password-reset/using-email", middlewares.LimitPerRoute("60-H"), middlewares.GuestJWT(), pwc.ResetByEmail)
 		}
 		// 注册一个路由
 		v1.GET("/", func(c *gin.Context) {
