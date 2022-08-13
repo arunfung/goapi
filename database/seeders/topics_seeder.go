1f0f549ba79aba30cab26b7d30d2410c4218c8eb
Arun Fung <arunfung@gmail.com>
2022-08-13 18:45:16 +0800

    话题列表

---
+++ database/seeders/topics_seeder.go
@@ -0,0 +1,28 @@
+package seeders
+
+import (
+	"fmt"
+	"goapi/database/factories"
+	"goapi/pkg/console"
+	"goapi/pkg/logger"
+	"goapi/pkg/seed"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	seed.Add("SeedTopicsTable", func(db *gorm.DB) {
+
+		topics := factories.MakeTopics(30)
+
+		result := db.Table("topics").Create(&topics)
+
+		if err := result.Error; err != nil {
+			logger.LogIf(err)
+			return
+		}
+
+		console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+	})
+}
